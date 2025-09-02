a941bbb9a7adacd270d0baeb822b65fe1d0170c1
metshehu <mettusha8@gmail.com>
2025-09-02 02:03:36 +0200

    cors

--- AISMARTSEARCH/MetiSearch/settings.py
+++ AISMARTSEARCH/MetiSearch/settings.py
@@ -73,9 +73,9 @@ MIDDLEWARE = [
 ]
 
 # === CORS ===
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",   # dev frontend
+    "http://localhost:8080",
+
     "https://168.231.81.181",  # prod frontend
 ]
 CORS_ALLOW_CREDENTIALS = True
